398e311d90de4e1657e16c27f24641510e92504f
Juan Cruz Gallart <gallartjuancruz@gmail.com>
2023-04-03 21:33:26 -0300

    PF Gallart

--- js/main.js
+++ js/main.js
@@ -1,11 +1,102 @@
 
+// let nombreUsuario = prompt("Ingrese su nombre");
 
-document.addEventListener("click",function (e){
-    if(e.target.classList.contains("gallery-item")){
-        const src = e.target.getAttribute("src");
-        document.querySelector(".modal-img").src = src;
-        const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'), options);
-        myModal.show();
+// let apellido = prompt("Ingrese su apellido")
 
+
+/*CONDICIONAL &&
+
+if((nombreUsuario !="")&&(apellido !="")){
+    alert("Nombre: " + nombreUsuario + "\nApellido: " + apellido)
+}
+else{
+    alert("Error: Ingresar nombre y apellido")
+}
+*/
+
+
+
+/*CONDICIONAL
+
+if (nombreUsuario == ""){
+    alert("No ingresaste un nombre de usuario")
+}
+else {
+    let decision = prompt ("Nombre ingresado: " + nombreUsuario + " \n desea cambiarlo? \n si \n no")
+    if (decision == "si"){
+        let nombreUsuario = prompt("Ingrese de nuevo su nombre")
+        alert ("Ha cambiado su nombre exitosamente. Bienvenido " + nombreUsuario)
     }
-})
+}
+*/
+
+// alert("Te vamos a hacer una breve encuesta")
+
+// let nombre = prompt ("Cual es tu nombre?")
+// let apellido = prompt ("Cual es tu apellido?")
+
+
+// if ((nombre && apellido) != ""){
+//     let edad = prompt ("Mucho gusto " + nombre + " " + apellido + "\n Cual es tu edad?")   
+//     if ((edad != "") && (edad >10) && (edad <50)){
+//         let direccion = prompt ("Ingrese su direccion de correo electronico")
+
+//         if(direccion != ""){
+//             alert ("Felicitaciones! Ha completado la encuesta exitosamente. \nSus datos son: \nNombre: " + nombre + "\nApellido: " + apellido + "\nEdad: " + edad + "\nDireccion de correo electronico: " + direccion)
+//         }
+//         else{
+//             alert ("Error, debe ingresar su correo, recargue la pagina y vuelva a intentarlo")
+//         }
+//     }
+//     else{
+//         alert ("Error, no ingreso su edad o no se encuentra dentro del rango etareo deseado (10 y 50 anios). En caso de haberse confundido recargue la pagina y vuelva a intentarlo")
+//     }
+// }
+// else{ alert ("Error, debe completar los campos 'nombre' y 'apellido'. Recargue la pagina y vuelva a intentarlo")
+    
+// }
+
+
+// let ingresarNum = parseInt (prompt ("ingresar num"));
+
+
+// for (let caca = 0; caca <= 10; caca++){
+
+//     if(caca == 6){
+//         break;
+//     }
+
+//     let resultado = ingresarNum * caca ;
+
+//     console.log(ingresarNum + "X" + caca + "=" + resultado)
+// }
+
+
+// alert ("Este es un formulario de registro")
+
+// let usuarioIngresado = prompt ("Ingrese su usario:")
+// let contraseniaIngresada = prompt ("Ingrese su contrasenia")
+
+
+// logInUser = prompt ("Inicie sesion con su usuario:")
+// logInContrasenia = prompt ("Inicie sesion con su contrasenia")
+
+// if ((logInUser == usuarioIngresado) && (logInContrasenia == contraseniaIngresada)){
+//     alert ("Ha iniciado sesion exitosamente")
+// }
+// else {
+//    alert ("Alguno de sus datos es incorrecto")
+// }
+
+// let dato = prompt ("Ingresa tus productos")
+
+// while (dato != "ESC"){
+//     console.log("El producto es " + dato )
+//     dato = prompt ("Ingresa tus productos")
+// }
+
+// function funcion () {
+//     alert("Eh?")
+// }
+
+// funcion ()
