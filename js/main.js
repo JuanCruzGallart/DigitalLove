94ec432b058c46e6b99a55a49325395f94f50862
JuanCruzGallart <gallartjuancruz@gmail.com>
2023-05-30 14:32:35 -0300

    777777

--- js/main.js
+++ js/main.js
@@ -7,12 +7,9 @@ const previewBox = document.querySelector(".preview-box");
 const previewImg = previewBox.querySelector(".imageClass");
 let prevBtn = document.querySelector(".prev");
 let nextBtn = document.querySelector(".next");
-<<<<<<< Updated upstream
 let cartNavBtn = document.querySelector(".nav_cart");
 let contenedorPortfolio = document.querySelector("#contenedor_portfolio");
 // let pageLoadShowFavs = document.addEventListener("DOMContentLoaded", )
-=======
->>>>>>> Stashed changes
 
 // Botones Aside
 let show2020Btn = document.getElementById("show_button2020");
@@ -24,18 +21,8 @@ let showTigreBtn = document.getElementById("tigre");
 // Array Favourites
 const imageArrayFavourites = [];
 
-<<<<<<< Updated upstream
 // ^_^ imgs
 loadFavImgs(`2022/^_^`, 1); loadFavImgs(`2022/^_^`,3 );loadFavImgs(`2022/^_^`, 2);loadFavImgs(`2022/^_^`, 4);loadFavImgs(`2022/^_^`, 5);loadFavImgs(`2022/^_^`, 8);loadFavImgs(`2022/^_^`, 11);loadFavImgs(`2022/^_^`, 13);loadFavImgs(`2022/^_^`, 41);loadFavImgs(`2022/^_^`, 26);loadFavImgs(`2022/^_^`, 31);loadFavImgs(`2022/^_^`, 40);
-=======
-<<<<<<< Updated upstream
-//Funcion CONSTRUCTORA
-function MyImage(src){
-    let img = new Image();
-    img.src = src;
-    return img;
-};
->>>>>>> Stashed changes
 
 //Tigre imgs
 loadFavImgs(`2022/tigre`,);
@@ -288,12 +275,12 @@ function removeCurtain() {
   cortina.removeChild(shpCrt);
 }
 
-cartNavBtn.addEventListener("click", function(){
+let btnAddToCart = document.querySelector(".preview_shoppingcrt");
+btnAddToCart.addEventListener("click", function addToCart(imagen) {
   
+})
 
-});
 
-<<<<<<< Updated upstream
 // document.addEventListener("DOMContentLoaded", function(){
 //   Swal.fire({
 //     title: 'Welcome to my portfolio!',
@@ -306,153 +293,3 @@ cartNavBtn.addEventListener("click", function(){
 //     }
 //   })
 // })
-=======
-//Array Favourites
-const imageArrayFavourites = [] ;
-
-//Array 2020
-=======
-// Array 2020
->>>>>>> Stashed changes
-const imageArray2020 = [];
-loadImages(`2020`, imageArray2020, 40);
-createImageElements(imageArray2020, show2020Btn);
-
-// Array 2021 firstOfTheYear
-const imageArray2021firstOfTheYear = [];
-loadImages(`2021/firstoftheyear`, imageArray2021firstOfTheYear, 30);
-createImageElements(imageArray2021firstOfTheYear, show2021firstOfTheYearBtn);
-
-// Array 2021fromThatNight FALTA COMPRIMIR
-const imageArray2021fromThatNight = [];
-loadImages(`2021/fromthatnight`, imageArray2021fromThatNight, 30);
-createImageElements(imageArray2021fromThatNight, show2021fromThatNightBtn);
-
-// Array ^_^
-const imageArray2022diciembre = [];
-loadImages(`2022/^_^`, imageArray2022diciembre, 52);
-createImageElements(imageArray2022diciembre, showDiciembreBtn);
-
-// Array Tigre
-const imageArrayTigre = [];
-loadImages(`2022/tigre`, imageArrayTigre, 42);
-createImageElements(imageArrayTigre, showTigreBtn);
-
-// Function CONSTRUCTORA
-function MyImage(src) {
-  let img = new Image();
-  img.src = src;
-  return img;
-}
-
-// Function that ITERATES through the photos in a selected folder and PUSHES them to a selected array
-function loadImages(carpeta, array, cantidad) {
-  for (let i = 1; i <= cantidad; i++) {
-    let img = new MyImage(`../resources/fotos__comprimidas/${carpeta}/${i}.jpg`);
-    img.onload = function () {
-      array.push(img);
-    };
-  }
-}
-
-// Function to create image elements and attach click event listener
-function createImageElements(imageSources, button) {
-  button.addEventListener("click", function () {
-    containerGaleria.innerHTML = "";
-
-    for (let i = 0; i < imageSources.length; i++) {
-      let span = document.createElement("span");
-      let img = document.createElement("img");
-
-      img.src = imageSources[i].src;
-      img.classList.add("imageClass");
-      img.addEventListener("click", function () {
-        lightbox(i, imageSources);
-      });
-
-      containerGaleria.appendChild(span);
-      span.appendChild(img);
-    }
-  });
-}
-
-// Function for the lightbox
-function lightbox(index, images) {
-  const previewImg = previewBox.querySelector("img");
-  const totalImages = images.length;
-  
-  function preview() {
-    let selectedImgUrl = images[index].src;
-    previewImg.src = selectedImgUrl;
-  }
-
-  preview();
-  previewBox.setAttribute("id", "show-box");
-  addCurtain();
-
-  document.addEventListener("click", function (event) {
-    const target = event.target;
-    if (target.classList.contains("curtain")) {
-      removeCurtain();
-    }
-  });
-
-  prevBtn.onclick = function () {
-    index--;
-    if (index < 0) {
-      index = totalImages - 1;
-    }
-    preview();
-  };
-
-  nextBtn.onclick = function () {
-    index++;
-    if (index >= totalImages) {
-      index = 0;
-    }
-    preview();
-  };
-
-  document.addEventListener("keydown" , function(event){
-    if(event.key === "ArrowLeft") {
-      index--;
-      if(index<0){
-        index = totalImages -1 ;
-      }
-      preview();
-    } else if(event.key === "ArrowRight"){
-      index++;
-      if (index>= totalImages){
-        index = 0;
-      }
-      preview();
-    }
-  })
-
-  document.addEventListener("keydown", function(event){
-    if(event.key === "Escape"){
-      removeCurtain();
-    }
-  })
-
-}
-
-// Functions for curtain
-function addCurtain() {
-  let curtain = document.createElement("div");
-  let navbar = document.getElementById("cortina-nav");
-  curtain.classList.add("curtain", "z-2");
-  document.getElementById("cortina").appendChild(curtain);
-  navbar.classList.add("hide");
-}
-
-function removeCurtain() {
-  let curtain = document.querySelector(".curtain");
-  if (curtain) {
-    curtain.parentNode.removeChild(curtain);
-    previewBox.removeAttribute("id");
-  }
-  let navbar = document.getElementById("cortina-nav");
-  navbar.classList.remove("hide");
-}
->>>>>>> Stashed changes
